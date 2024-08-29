19339facd3e657ae2690108cfcad295a1c95cb84
watersoup <jrangrej@hotmail.com>
2024-08-28 23:27:25 -0400

    Works W/OTA_webserver  and inline html

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <bleClientObj.h>
 #include <BLEDevice.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPAsyncWiFiManager.h> 
+#include <ElegantOTA.h>
+#include "wifiData.h"
+
+
+
+AsyncWebServer server(80);
+DNSServer dnsServer;
+AsyncWebSocket ws("/ws");
+
+AsyncWiFiManager *wifiManager = new AsyncWiFiManager(&server, &dnsServer);
+
+unsigned long ota_progress_millis = 0;
 int BUILTINPIN = 8;
 int count =0;
 bleClientObj *bleInst=nullptr;
@@ -16,13 +32,75 @@ void blink(const int PIN, int times=1, long int freq=500){
   }
 }
 
+void onOTAStart() {
+  // Log when OTA has started
+  Serial.println("OTA update started!");
+  // <Add your own code here>
+}
 
+void onOTAProgress(size_t current, size_t final) {
+  // Log every 1 second
+  if (millis() - ota_progress_millis > 1000) {
+    ota_progress_millis = millis();
+    Serial.printf("OTA Progress Current: %u bytes, Final: %u bytes\n", current, final);
+  }
+}
+
+void onOTAEnd(bool success) {
+  // Log when OTA has finished
+  if (success) {
+    Serial.println("OTA update finished successfully!");
+  } else {
+    Serial.println("There was an error during OTA update!");
+  }
+  // <Add your own code here>
+}
+void setOTA(){
+  // set up OTA
+  ElegantOTA.begin(&server);
+  ElegantOTA.setAuth("Admin", "admin");
+  ElegantOTA.onStart(onOTAStart);
+  ElegantOTA.onProgress(onOTAProgress);
+  ElegantOTA.onEnd(onOTAEnd);
+}
+void startWifi(){
+
+  WiFi.begin(SSID, PASS_WD);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+    Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 void setup()
 {
+    // start WIFI;
+    blink(BUILTINPIN, 1, 1000);
     bleInst = new bleClientObj();
-    pinMode(BUILTINPIN,OUTPUT);
-    blink(BUILTINPIN,2,1000);
     Serial.begin(115200);
+    pinMode(BUILTINPIN,OUTPUT);
+    blink(BUILTINPIN,2,200);
+    startWifi();
+    blink(BUILTINPIN,2,1000);
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+        request->send(200,(const char *)"text/html", (const char *)indexpage, nullptr);
+    });
+
+    blink(BUILTINPIN,4,200);
+    setOTA();
+    blink(BUILTINPIN,2,1000);
+    blink(BUILTINPIN,5,200);
+    server.begin();
+    blink(BUILTINPIN,2,1000);
+
     Serial.println("Starting Arduino BLE Client application...");
     bleInst->scan();
     blink(BUILTINPIN,10,200);
